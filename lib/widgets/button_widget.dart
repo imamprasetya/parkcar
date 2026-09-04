a6d763c3e584c5669a59e50d7e4165fc23468918
imamprasetya <imamprasetya06@gmail.com>
2026-09-04 19:23:08 +0700

    button widget

--- lib/widgets/button_widget.dart
+++ lib/widgets/button_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:parkcar/theme/app_pallete.dart';
 
 class ButtonWidget extends StatelessWidget {
@@ -9,7 +10,17 @@ class ButtonWidget extends StatelessWidget {
     return ElevatedButton(
       onPressed: () {},
       style: ElevatedButton.styleFrom(
+        elevation: 0,
+        backgroundColor: AppPallete.white,
         foregroundColor: AppPallete.colorPrimaryDark,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 12,
+        ),
+        textStyle: GoogleFonts.plusJakartaSans(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+        ),
       ),
       child: Text("Open Maps"),
     );
